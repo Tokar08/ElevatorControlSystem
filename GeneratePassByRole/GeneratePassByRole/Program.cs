070d36ffb78fdc657ce4b44d486c851553657f42
UAVMK <ytfir3fly@gmail.com>
2023-07-26 21:36:51 +0300

    update full project
    Залил все части проекта. Старую, новую, активность_онлайн, генератор паролей и всё остальное

---
+++ GeneratePassByRole/GeneratePassByRole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+
+    public static string GeneratePass(string postfix)
+    {
+        const string allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+       
+        char[] password = new char[8];
+
+        for (int i = 0; i < 7; i++)
+        {
+            password[i] = allowedChars[new Random().Next(allowedChars.Length)];
+        }
+
+        return new string(password) + postfix;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(GeneratePass("_Bss"));
+        Console.WriteLine(GeneratePass("_Bss"));
+        Console.WriteLine(GeneratePass("_Bss"));
+
+        Console.WriteLine();
+
+
+        Console.WriteLine(GeneratePass("_Adm"));
+        Console.WriteLine(GeneratePass("_Adm"));
+
+        Console.ReadLine();
+    }
+}
---
+++ TestOnlineOrNot/TestOnlineOrNot/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WindowsInput;
+
+namespace TestOnlineOrNot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isUserActive;
+        private DateTime lastActiveTime;
+        private DispatcherTimer activityTimer;
+        //Время для того что бы пользователь стал "неактивен"
+        private int inactivityTimeInSeconds = 10;
+
+        //Время для того что бы пользователь стал "не в сети"
+        private int offlineTimeInSeconds = 20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            isUserActive = false;
+            lastActiveTime = DateTime.Now;
+
+          
+            Application.Current.MainWindow.MouseMove += MainWindow_MouseMove;
+            Application.Current.MainWindow.MouseWheel += MainWindow_MouseWheel;
+            Application.Current.MainWindow.PreviewKeyDown += MainWindow_PreviewKeyDown;
+
+            // Инициализация таймера активности
+            activityTimer = new DispatcherTimer();
+            activityTimer.Interval = TimeSpan.FromSeconds(1); // Проверяем активность каждую секунду
+            activityTimer.Tick += ActivityTimer_Tick;
+            activityTimer.Start();
+        }
+
+        private void ActivityTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timeSinceLastActive = DateTime.Now - lastActiveTime;
+            if (timeSinceLastActive.TotalSeconds < inactivityTimeInSeconds)
+            {
+          
+                StatusTextBlock.Text = "Пользователь активен";
+            }
+            else if (timeSinceLastActive.TotalSeconds >= inactivityTimeInSeconds && timeSinceLastActive.TotalSeconds < offlineTimeInSeconds)
+            {
+                StatusTextBlock.Text = "Пользователь неактивен";
+            }
+            else
+            {
+                StatusTextBlock.Text = "Пользователь не в сети";
+            }
+        }
+
+        private void UpdateLastActiveTime()
+        {
+            lastActiveTime = DateTime.Now;
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            isUserActive = true;
+            UpdateLastActiveTime();
+        }
+
+        private void MainWindow_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            isUserActive = true;
+            UpdateLastActiveTime();
+        }
+
+        private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            isUserActive = true;
+            UpdateLastActiveTime();
+        }
+    }
+}
